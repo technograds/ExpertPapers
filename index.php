876ca1ad008faf8aca8b5dd00c2e4e28d692a72a
Muavia Ejaz Usmani <meusmani@yahoo.com>
2013-06-17 01:37:26 +0500

    Repository created

---
+++ index.php
@@ -0,0 +1,154 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <!--TITLE OF THE SITE GOES HERE-->
+        <title>Expert Papers</title>
+
+        <!--META TAGS GO HERE-->
+        <meta charset="utf-8" />
+        <meta name="description" content="Place to get creative written content by expert hands" />
+        <meta name="keywords" content="expert, papers, undergrad, high, school, masters, doctoral, writers, custom, services, literature" />
+		<meta name="author" content="Created by TechnoGrads" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+
+        <!--FAVICON-->
+		<link rel="icon" type="image/png" href="#" />
+
+        <!--CSS STYLESHEETS GO HERE-->
+        <link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700,400italic,500italic' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Raleway:400,500,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Almendra:400,700italic,700,400italic' rel='stylesheet' type='text/css'>
+
+        <!--JAVASCRIPT GOES HERE-->
+        
+
+    </head>
+
+    <body>
+        
+        <!--HEADER SECTION-->
+        <header>
+
+            <ul id="navigation">
+
+                <li><a  href="#">Home</a></li>
+                <li><a  href="#">Pricing</a></li>
+                <li><a  href="#">FAQ</a></li>
+                <li><a  href="#">Our Writers</a></li>
+                <li><a href="#" style="color: #CD0000;">Order Now</a></li>
+
+            </ul>
+
+        </header>
+
+        <!--BODY SECTION-->
+        <section id="main-wrapper">
+
+            <div id="content-wrapper">
+
+                <div id="content-header">
+
+                    <div id="logo-box">
+
+                        <div id="logo">
+
+                            <h1>Expert Papers</h1>
+
+                        </div>
+
+                        <div id="order-and-inquire">
+
+                            <a href="#" id="inquiry">Free Inquiry</a>
+                            <a href="#" style="background: #000000;" id="order">Order Now</a>
+
+                        </div>
+                    
+                    </div>
+
+                </div>
+
+                <div id="intro-container">
+
+                    <h1>Professional Essay Writers</h1>
+
+                    <p id="main-text">During their academic years, students are required to write essays as a part of their learning programs. Typically, a professional essay 
+                        includes three parts – an introduction paragraph, the body of the essay, and the concluding portion. The introductory paragraph should 
+                        give an idea to the reader about the topic of the essay. The purpose of the essay can also be mentioned in the initial paragraph. You 
+                        may need the help of professional essay writers now!
+                    </p>
+
+                    <div id="sub-data-container">
+
+                        <div class="sub-data">
+                        
+                            <h3>Professionals Know the Structure</h3>
+
+                            <p>
+                                The body of the essay is considered as its main part and here the writer has to explain his or her viewpoints about the subject 
+                                and the evidences to support those opinions. The concluding part should contain all the major points of the essay. The writer 
+                                has the right to put some suggestions or warnings in this paragraph.
+                            </p>
+                        
+                        </div>
+
+                        <div class="sub-data">
+                        
+                            <h3>Well Organized</h3>
+
+                            <p>
+                                If you want to write an essay, the first thing you have to do is to collect information about the topic. Writing a professional 
+                                essay involves a lot of reading, thinking, and drawing conclusions or findings from the available data. It is very important that 
+                                the essay has to be presented in a well-organized manner. It should be written in good English and without any mistakes. Each 
+                                sentence must be well structured and must have a clearly expressed meaning. The professional essay writers should never use 
+                                unnecessary words or unambiguous sentences.
+                            </p>
+                        
+                        </div>
+
+                        <div class="sub-data" style="margin-right: 0;">
+                        
+                            <h3>Get Assistance</h3>
+
+                            <p>
+                                The students who are not comfortable with writing essays can get the help of a professional essay writers. There are many 
+                                writing services that offer help in writing services. However, it is important to decide on a service that has talented best 
+                                essay writers who can writer original and informative essays.
+                            </p>
+                        
+                        </div>
+
+                    </div>
+                
+                </div>
+
+                <div id="price-calc">
+                
+                    <h1>Price Calculator</h1>
+
+
+                
+                </div>
+
+            </div>
+
+        </section>
+
+        <footer>
+
+            
+
+        </footer>
+
+    </body>
+
+</html>
